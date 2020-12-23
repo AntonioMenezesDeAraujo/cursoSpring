3f8a9c39e51d4238ee02cd448606b55042ba8ede
AntonioMAraujo <netynho_antonio@hotmail.com>
2020-12-23 20:43:40 -0300

    Comit das entidades

---
+++ src/main/java/com/cursospringboot/bookstoragemanager/books/entity/Book.java
@@ -0,0 +1,45 @@
+package com.cursospringboot.bookstoragemanager.books.entity;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import com.cursospringboot.bookstoragemanager.author.entity.Author;
+import com.cursospringboot.bookstoragemanager.publishers.entity.Publisher;
+import com.cursospringboot.bookstoragemanager.users.entity.User;
+
+import lombok.Data;
+
+@Data
+@Entity
+public class Book {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false, length = 100)
+	private String name;
+
+	@Column(nullable = false)
+	private String isbn;
+
+	@Column(columnDefinition = "integer default 0")
+	private int pages;
+
+	@Column(columnDefinition = "integer default 0")
+	private int chapters;
+
+	@ManyToOne(cascade = { CascadeType.MERGE })
+	private Author author;
+	
+	@ManyToOne(cascade = { CascadeType.MERGE })
+	private Publisher publisher;
+	
+	@ManyToOne(cascade = { CascadeType.MERGE })
+	private User user;
+}
